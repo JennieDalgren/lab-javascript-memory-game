e374cc5ed17deb068d8b242721186c943d6fb5cc
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-08-23 19:46:44 +0200

    Need to fix shuffle and to not be able to click 3 cards

--- starter-code/memory.js
+++ starter-code/memory.js
@@ -1,6 +1,6 @@
-// //******************************************************************
-// // Game Logic
-// //******************************************************************
+//******************************************************************
+// Game Logic
+//******************************************************************
 var MemoryGame = function() {
   this.cards = [
   		{ name: "aquaman",         img: "aquaman.jpg" },
@@ -33,11 +33,13 @@ var MemoryGame = function() {
     this.correctPairs = 0;
 };
 
+
 // //******************************************************************
 // // HTML/CSS Interactions
 // //******************************************************************
 
 var memoryGame;
+
 
 $(document).ready(function(){
   memoryGame = new MemoryGame();
@@ -46,7 +48,7 @@ $(document).ready(function(){
   memoryGame.cards.forEach(function(pic, index) {
     var sanitizedName = pic.name.split(' ').join('_');
 
-   html += '<div class= "card" name="card_' + sanitizedName + '">';
+    html += '<div class= "card" name="card_' + sanitizedName + '">';
     html += '<div class="back"';
     html += '    name="' + pic.name + '">';
     html += '</div>';
@@ -57,6 +59,83 @@ $(document).ready(function(){
     html += '</div>';
   });
 
-  // Add all the divs to the HTML
-  document.getElementById('memory_board').innerHTML = html;
+    // Add all the divs to the HTML
+    document.getElementById('memory_board').innerHTML = html;
+
+
+
+    $(".front").hide();
+
+
+    $('.back').on('click', function() {
+      var name = $(this).attr('name');
+      console.log(name);
+      $(this).hide();
+      $(this).siblings().show();
+
+      console.log(memoryGame.selectedCards);
+
+      if (memoryGame.selectedCards.length === 0){
+        memoryGame.selectedCards.push(name);
+        $(".front,.back").addClass('blocked');
+
+      } else if (memoryGame.selectedCards.length === 1){
+        memoryGame.selectedCards.push(name);
+        console.log('two!');
+        // $('div').toggleClass('blocked'); ///How to make it so can't click more than two cards.
+        checkCards();
+      }
+    });
+
+    //Check if the cards matches
+    function checkCards() {
+      setTimeout(function() {
+        if (memoryGame.selectedCards[0] === memoryGame.selectedCards[1]) {
+          console.log('match!');
+          pairScore();
+          pairsClickedCount();
+          checkEndGame();
+        } else {
+          memoryGame.selectedCards.forEach(function (cardName) {
+            console.log($('[name="' + cardName + '"].front'));
+            $('[name="' + cardName + '"].back').show();
+            $('[name="' + cardName + '"].front').hide();
+          });
+          pairsClickedCount();
+        }
+        memoryGame.selectedCards = [];
+      }, 1000);
+    }
+
+    function pairScore(){
+        memoryGame.correctPairs++ ;
+        console.log("new score: " + memoryGame.correctPairs);
+        $("#pairs_correct").html(memoryGame.correctPairs);
+      }
+
+    function pairsClickedCount(){
+        memoryGame.pairsClicked++ ;
+        console.log("pairs clickes: " + memoryGame.pairsClicked);
+        $("#pairs_clicked").html(memoryGame.pairsClicked);
+      }
+
+    function checkEndGame() {
+      if (memoryGame.correctPairs === 12){
+        alert("WINNING! You found all the pairs in just " + memoryGame.pairsClicked + " attempts.");
+      }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
